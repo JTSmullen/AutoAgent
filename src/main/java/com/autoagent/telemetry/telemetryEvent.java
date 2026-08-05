560f3b0a9652ab240de9c33f0e4b708a9e497971
Smullen <smullenjt23@gcc.edu>
2026-08-05 11:13:41 -0400

    [Add] -> Telemetry Kafka Event DTO

---
+++ src/main/java/com/autoagent/telemetry/telemetryEvent.java
@@ -0,0 +1,17 @@
+package com.autoagent.telemetry;
+
+import java.time.Instant;
+
+/**
+ * Standardized time series event to be sent to kafka
+ */
+public record telemetryEvent (
+
+    String satelliteId,
+    Instant timestamp,
+    double latitude,
+    double longitude,
+    double altitude,
+    double velocity
+
+) {}
