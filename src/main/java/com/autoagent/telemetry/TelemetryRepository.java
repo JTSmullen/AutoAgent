0108bf6bf28f53b11f2edaf910ff725a9a41a927
Smullen <smullenjt23@gcc.edu>
2026-08-13 16:14:57 -0400

    [Add] -> Time Series Queries

--- src/main/java/com/autoagent/telemetry/TelemetryRepository.java
+++ src/main/java/com/autoagent/telemetry/TelemetryRepository.java
@@ -3,9 +3,21 @@ package com.autoagent.telemetry;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface TelemetryRepository extends JpaRepository<TelemetryEntity, Long> {
 
+    Optional<TelemetryEntity> findFirstBySatelliteIdOrderByIdTimestampDesc(
+            String satelliteId
+    );
 
+    List<TelemetryEntity> findBySatelliteIdAndIdTimestampBetweenOrderByIdTimestampAsc(
+            String satelliteId,
+            Instant startTime,
+            Instant endTime
+    );
 
 }
