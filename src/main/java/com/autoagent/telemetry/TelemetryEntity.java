60915d100c6bfae549f993b7179ae9319953f6d9
Smullen <smullenjt23@gcc.edu>
2026-08-13 14:47:40 -0400

    [Add] -> Telemetry Entity to save to time series db

---
+++ src/main/java/com/autoagent/telemetry/TelemetryEntity.java
@@ -0,0 +1,25 @@
+package com.autoagent.telemetry;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.Instant;
+
+@Entity
+@Getter
+@Setter
+public class TelemetryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "tel_id", nullable = false)
+    private Long telId;
+
+    private String satelliteId;
+    private Instant timestamp;
+    private double latitude;
+    private double longitude;
+    private double altitude;
+    private double velocity;
+
+}
